5704ec8df59e83b7f2555e1a81286c200485ad9f
U-CMAI-ASUS\Chris <rokuren.m@gmail.com>
2015-12-16 12:27:18 -0500

    Initial add

---
+++ include/Server.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef SERVER_H
+#define SERVER_H
+#include <cpprest/http_listener.h>
+#include <iostream>
+#include <complex>
+#include <string.h>
+#include <chrono>
+#include <thread>
+#include "lodepng.h"
+/*
+void handle_get(web::http::http_request);
+void handle_post(web::http::http_request);
+float pack_color(unsigned char rgba[4]);
+
+struct ImageArray {
+	int32_t number_of_dimensions;
+	int32_t dimensions[100]; // width ?
+	std::complex<float> data[120]; // height ?
+
+	ImageArray() {
+		number_of_dimensions = 2;
+	}
+};
+
+struct Date {
+	int month;
+	int day;
+	int year;
+
+	Date() {
+		month = 1;
+		day = 5;
+		year = 1989;
+	}
+};
+*/
+#endif
---
+++ src/Server.cpp
@@ -0,0 +1,36 @@
+
+#include "server.h"
+
+using namespace std;
+/*using namespace web;
+using namespace web::http;
+using namespace web::http::experimental::listener; // HTTP Server
+*/
+int main() {
+	/*
+	http_listener listener(L"http://localhost:18080/transform");
+
+	listener.support(methods::GET, handle_get);
+	listener.support(methods::POST, handle_post);
+	//listener.support(methods::PUT, handle_put);
+	//listener.support(methods::DEL, handle_del);
+
+	try {
+		listener.open().then([&listener]() {
+			ucout << "Server Started\n";
+		}).wait();
+		
+		while (true);
+	}
+	catch (http_exception e){
+		listener.close();
+		ucout << "An exception occured. Could not open listener";
+	}
+	
+
+
+	listener.close();
+*/
+    std::cout << "Hello,World";
+	return 0;
+}
